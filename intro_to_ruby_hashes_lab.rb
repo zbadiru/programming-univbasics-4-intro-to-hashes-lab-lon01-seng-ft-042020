857a50256f542aa95cba0176365b387db7c6550b
Zakari Badiru <zbadiru@live.co.uk>
2020-03-30 14:08:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,11 @@ def update_counting_hash(hash, key)
     hash[key] += 1 
   else
     hash[key] = 1
+  end 
+  hash
+end
   
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-end
+  
