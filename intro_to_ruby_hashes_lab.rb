ca249bb1ac9b4d60b5064088e306786db3518a5f
Zakari Badiru <zbadiru@live.co.uk>
2020-03-30 14:06:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,9 @@ end
 
 def update_counting_hash(hash, key) 
   if hash[key]
-    hash[key] + 1 
+    hash[key] += 1 
+  else
+    
 
   
   # given a hash an a key as parameters, return an updated hash
