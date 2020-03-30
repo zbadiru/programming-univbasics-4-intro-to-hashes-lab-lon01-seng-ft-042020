fe68d646ab1246e2d97f22964674064a7c01a42d
Zakari Badiru <zbadiru@live.co.uk>
2020-03-30 14:16:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,8 @@ def my_hash_creator(key, value)
   }
   # return a hash that includes the key and value parameters passed into this method
 end
+my_hash_creator
+end
 
 def read_from_hash(hash, key)
   hash[key]
