11f2477cf2dc1c145fa3456a6f8955417b44aeca
Zakari Badiru <zbadiru@live.co.uk>
2020-03-30 14:10:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hash[:name => Grace Hopper]
+  my_hash_creator[:name => "Grace Hopper"]
   # return a hash that includes the key and value parameters passed into this method
 end
 
